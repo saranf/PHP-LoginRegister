c46a1dfc0392b1eb16fac0efdd295cd17647b046
saranf <qorwlsdk1995@naver.com>
2019-07-05 15:54:11 +0900

    postgresql 간단 사용법 주석 커밋

--- Login_postgresql.php
+++ Login_postgresql.php
@@ -6,5 +6,10 @@
  *
  *  Login.php, Register.php 이 두개는 mysql로 짜여 있는 파일이며
  *  Login_postgresql.php Register_postgresql.php이 두개는 postgresql 로 짜여 있는 파일이다.
+ *
+ *  postgresql 간단 사용법
+ *
+ *  1. sudo su - postgres  => postgres 유저 접근
+ *  2. psql => postgresql 접속
  */
 ?>

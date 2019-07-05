feda2885c6f9e7322bcaee4f2b16c36a17fec885
saranf <qorwlsdk1995@naver.com>
2019-07-05 15:28:21 +0900

    주석 추가 커밋

---
+++ Login_postgresql.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ *  설치 방법
+ *  sudo apt install postgresql
+ *  sudo apt install php-pgsql
+ *
+ *  Login.php, Register.php 이 두개는 mysql로 짜여 있는 파일이며
+ *  Login_postgresql.php Register_postgresql.php이 두개는 postgresql 로 짜여 있는 파일이다.
+ */
+?>
